2686bd1e56c0db5499d39284d8fb3b2093cd6448
Erik-MonkeyProof <erik.grassens@monkeyproofsolutions.nl>
2024-01-16 15:32:23 +0100

    Update runCc4m.m
    Undo debug stuff.

--- runCc4m.m
+++ runCc4m.m
@@ -1,12 +1,7 @@
 function runCc4m(downloadLocation)
     filePath = websave('CC4M.mltbx', downloadLocation);
     matlab.addons.install(filePath);
-    try
-        monkeyproof.codetools.CodeAnalyzer.LicenseChecker.checkout()
-    catch ME
-        fprintf(1, '%s\n', ME.getReport());
-    end
-    %sarif = monkeyproof.cc4m.start('-pwd', '-recursive', '-generateSarif');
-    %movefile(sarif, 'report.sarif.json');
-    %exit(0);
+    sarif = monkeyproof.cc4m.start('-pwd', '-recursive', '-generateSarif');
+    movefile(sarif, 'report.sarif.json');
+    exit(0);
 end
